d92c97a04d24692728b5bf7552608393033e3f19
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-28 16:31:19 -0700

    Added hero and made him draggable

--- client/watchout.js
+++ client/watchout.js
@@ -2,37 +2,45 @@
 
 var width = 500; // enemy width, height
 var height = 500;
-var dataset = [[1, 1],[2,2],[3, 3], [4,4] ,[5,5]]; //dataset.length -> number of asteroids
+var dataset = []; //dataset.length -> number of asteroids
 var gameBoard = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
     
-gameBoard.append("circle")
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .attr("r", 100)
-    .attr("fill", "orange");
 
-// var enemy = d3.select("svg").selectAll("enemy")
-//   .data([1])
-//   .enter()
-//   .append("div")
-//   .attr("class", "enemy");
+ gameBoard.append("circle")
+    .attr("class", "hero")
+    .attr("cx", width/2)
+    .attr("cy", height/2)
+    .attr("r", 20)
+    .attr("fill", "orange")
+    // .call(drag);
 
-var drawElements = function() {
+var hero = gameBoard.select("circle");
+var drag = d3.behavior.drag()
+  .on("drag", function(d, i) {
+    d3.select(this).attr("cx", d3.event.x)
+    .attr("cy", d3.event.y);
+    
+  });
+hero.call(drag);
+
+var createElements = function(n) {
+  for (var i = 0; i < n; i++) {
+    dataset.push(randomLocation());
+  }
   gameBoard.selectAll("image")
     .data(dataset)
-    // .transition()
-    .attr("xlink:href","asteroid.png")
-    .attr("width", "50")
-    .attr("height", "50")
-    .attr("x", function(d){ return d[0] })
-    .attr("y", function(d) {return d[1]})
     .enter()
     .append("image")
+    .attr("xlink:href","asteroid.png")
+    .attr("width", "25")
+    .attr("height", "25")
+    .attr("x", function(d){ return d[0] })
+    .attr("y", function(d) {return d[1]})
     .text(function(d) {return d});
 }
-drawElements();
+createElements(20);
 
 function randomLocation() {
   var x = Math.floor(Math.random() * (width - 50));
@@ -41,30 +49,15 @@ function randomLocation() {
 }
 
 setInterval(function() {
-  //modify dataset, computing random locations for each element
-  //then call drawElements with new dataset
   for (var i = 0; i < dataset.length; i++) {
     dataset[i] = randomLocation();
   }
   d3.selectAll("image")
     .data(dataset)
     .transition()
-    .attr("xlink:href","asteroid.png")
-    .attr("width", "50")
-    .attr("height", "50")
     .attr("x", function(d){ return d[0] })
     .attr("y", function(d) {return d[1]})
-  // drawElements(dataset);
+   
 }, 1000);
 
-
-  // gameBoard.selectAll("circle")
-  //   .data([5, 2])
-  //   .attr("fill","green")
-  //   .enter()
-  //   .append("circle")
-  //   .attr("cx", 50)
-  //   .attr("cy", 50)
-  //   .attr("r", 50)
-  //   .text(function(d) {return d});
 
