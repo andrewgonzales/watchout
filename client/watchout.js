5c60b3530bc039f77b3fd0d8e3e30ae2d0d6027c
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-29 16:46:39 -0700

    Added image files for player, added background, fixed player movement functionality, refactored code

--- client/watchout.js
+++ client/watchout.js
@@ -1,34 +1,75 @@
 // start slingin' some d3 here.
 
-var width = 500; // enemy width, height
-var height = 500;
+var width = 600; // enemy width, height
+var height = 600;
 var enemyRadius = 25;
-var heroRadius = 20;
+var heroSize = 40;
 var currentScore = 0;
 var highScore = 0;
 var collisionCount = 0;
 var enemyCount = 20;
 var collisionFlag = false;
+var interval = 2500;
+var marioCharacters = [
+  [ "toad_still.png", "toad_spinning.gif"],
+  [ "mario_still.png", "mario_spinning.gif"],
+  [ "koopa_still.png", "koopa_spinning.gif"],
+  [ "peach_still.png", "peach_spinning.gif"],
+  [ "luigi_still.png", "luigi_spinning.gif"],
+  [ "donkey_kong_still.png", "donkey_kong_spinning.gif"],
+  [ "bowser_still.png", "bowser_spinning.gif"],
+  [ "yoshi_still.png", "yoshi_spinning.gif"]
+];
+var marioEnemy = "red_shell.gif";
 
+var heroCharacters = [
+["hero_animated.gif", "hero_hit.gif"]
+];
+var heroEnemy;
+
+var currentCharacter = getRandomChar(marioCharacters);
+var currentEnemy = marioEnemy;
+function getRandomChar(characters) {
+  return characters[Math.floor(Math.random()* characters.length)];
+}
 var enemyCoords = []; //enemyCoords.length -> number of asteroids
 var gameBoard = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
     
 
- gameBoard.append("circle")
+ gameBoard.append("image")
     .attr("class", "hero")
-    .attr("cx", width/2)
-    .attr("cy", height/2)
-    .attr("r", heroRadius)
-    .attr("fill", "green")
-    // .call(drag);
+    .attr("x", width/2)
+    .attr("y", height/2)
+    .attr("width", heroSize)
+    .attr("height", heroSize)
+    .attr("xlink:href", 'images/' + currentCharacter[0]);
 
-var hero = gameBoard.select("circle");
+var hero = gameBoard.select(".hero");
 var drag = d3.behavior.drag()
   .on("drag", function(d, i) {
-    d3.select(this).attr("cx", d3.event.x)
-    .attr("cy", d3.event.y);
+    var newX = d3.event.x - heroSize/2;
+    var newY = d3.event.y - heroSize/2;
+    if (d3.event.x - heroSize/2 < 0){
+      newX = 0;
+    }
+    
+    if (d3.event.x + heroSize/2 > d3.select("svg").attr("x") + width) {
+      newX = d3.select("svg").attr("x") + width - heroSize;
+    }
+
+    if (d3.event.y - heroSize/2< 0) {
+      newY = 0;
+    }
+
+    if (d3.event.y + heroSize/2 > d3.select("svg").attr("y") + height) {
+      newY = d3.select("svg").attr("y") + height - heroSize;
+    }
+
+    d3.select(this).attr("x", newX);
+    d3.select(this).attr("y", newY);
+
     
   });
 hero.call(drag);
@@ -42,18 +83,17 @@ var createEnemies = function(n) {
     .enter()
     .append("image")
     .attr("class", "enemy")
-    .attr("xlink:href","asteroid.png")
+    .attr("xlink:href","images/" + currentEnemy)
     .attr("width", enemyRadius)
     .attr("height", enemyRadius)
-    .attr("x", function(d){ return d[0] })
-    .attr("y", function(d) {return d[1]})
-    // .text(function(d) {return d});
-}
+    .attr("x", function(d){ return d[0];})
+    .attr("y", function(d) {return d[1];});
+};
 createEnemies(enemyCount);
 
 function randomLocation() {
-  var x = Math.floor(Math.random() * (width - 50));
-  var y = Math.floor(Math.random() * (height - 50));
+  var x = Math.floor(Math.random() * (width));
+  var y = Math.floor(Math.random() * (height));
   var speed = Math.floor(Math.random() * 4);
   return [x, y, speed];
 }
@@ -62,36 +102,39 @@ setInterval(function() {
   for (var i = 0; i < enemyCoords.length; i++) {
     enemyCoords[i] = randomLocation();
   }
-  d3.selectAll("image")
+  d3.selectAll(".enemy")
     .data(enemyCoords)
     .transition()
-    .duration(1500)
-    .attr("x", function(d){ return d[0] })
-    .attr("y", function(d) {return d[1]});
+    .duration(interval)
+    .attr("x", function(d){ return d[0];})
+    .attr("y", function(d) {return d[1];});
    
   if(collisionFlag){
-
     collisionFlag = false;
+    currentCharacter = getRandomChar(marioCharacters);
+    d3.select(".hero")
+      .attr("xlink:href",'images/' + currentCharacter[0]);
   }
-}, 1500);
+}, interval);
 
 
 setInterval(function(){
   //check each asteroid position for collision
   var currentDistance;
-  var nodes = d3.selectAll("image");
+  var nodes = d3.selectAll(".enemy");
   hero.attr("fill", "green");
-  d3.selectAll("image")
+  d3.selectAll(".enemy")
     .each(function(d, i) {
       var x = d3.select(this).attr("x");
       var y = d3.select(this).attr("y");
       currentDistance = distance(x, y);
-      if (currentDistance < enemyRadius + heroRadius - 10) {
+      if (currentDistance < 20) {
         hero.attr("fill", "red");
         if (!collisionFlag) { //first collision in the 1.5 second interval
           collisionCount++;
           d3.select(".collisionCount").text(collisionCount);
-
+          d3.select(".hero")
+            .attr("xlink:href", 'images/' + currentCharacter[1]);
         }
         currentScore = 0;
         d3.select(".currentScore").text(currentScore);
@@ -102,28 +145,22 @@ setInterval(function(){
 }, 10);
 
 setInterval(function(){
-  
-  // if(collisionFlag){
-  //   currentScore = 0;
-  // } else {
   currentScore++;
   if(currentScore > highScore){
     highScore = currentScore;
     d3.select(".highScore").text(highScore);
-
-    // }
   }
   d3.select(".currentScore").text(currentScore);
 
 }, 10);
 
 var distance = function(enemyX, enemyY){
-  var heroX = hero.attr("cx");
-  var heroY = hero.attr("cy");
+  var heroX = hero.attr("x");
+  var heroY = hero.attr("y");
   var a = Math.pow((heroX - enemyX), 2);
   var b = Math.pow((heroY - enemyY), 2);
   return Math.sqrt(a + b);
-}
+};
 
 
 
