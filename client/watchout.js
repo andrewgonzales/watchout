bcf406841f152ad8258e3658c6479431c5ca1bc3
andrewgonzales <andrewmartingonzales@gmail.com>
2015-08-28 19:45:33 -0700

    Added collision handling

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,13 @@
 
 var width = 500; // enemy width, height
 var height = 500;
-var dataset = []; //dataset.length -> number of asteroids
+var enemyRadius = 25;
+var heroRadius = 20;
+var collisionCount = 0;
+var enemyCount = 20;
+var collisionFlag = false;
+
+var enemyCoords = []; //enemyCoords.length -> number of asteroids
 var gameBoard = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -12,7 +18,7 @@ var gameBoard = d3.select("body").append("svg")
     .attr("class", "hero")
     .attr("cx", width/2)
     .attr("cy", height/2)
-    .attr("r", 20)
+    .attr("r", heroRadius)
     .attr("fill", "orange")
     // .call(drag);
 
@@ -25,22 +31,23 @@ var drag = d3.behavior.drag()
   });
 hero.call(drag);
 
-var createElements = function(n) {
+var createEnemies = function(n) {
   for (var i = 0; i < n; i++) {
-    dataset.push(randomLocation());
+    enemyCoords.push(randomLocation());
   }
   gameBoard.selectAll("image")
-    .data(dataset)
+    .data(enemyCoords)
     .enter()
     .append("image")
+    .attr("class", "enemy")
     .attr("xlink:href","asteroid.png")
-    .attr("width", "25")
-    .attr("height", "25")
+    .attr("width", enemyRadius)
+    .attr("height", enemyRadius)
     .attr("x", function(d){ return d[0] })
     .attr("y", function(d) {return d[1]})
-    .text(function(d) {return d});
+    // .text(function(d) {return d});
 }
-createElements(20);
+createEnemies(enemyCount);
 
 function randomLocation() {
   var x = Math.floor(Math.random() * (width - 50));
@@ -49,15 +56,53 @@ function randomLocation() {
 }
 
 setInterval(function() {
-  for (var i = 0; i < dataset.length; i++) {
-    dataset[i] = randomLocation();
+  for (var i = 0; i < enemyCoords.length; i++) {
+    enemyCoords[i] = randomLocation();
   }
   d3.selectAll("image")
-    .data(dataset)
+    .data(enemyCoords)
     .transition()
+    .duration(1500)
     .attr("x", function(d){ return d[0] })
-    .attr("y", function(d) {return d[1]})
+    .attr("y", function(d) {return d[1]});
    
-}, 1000);
+  if(collisionFlag){
+
+    collisionFlag = false;
+  }
+}, 1500);
+
+
+setInterval(function(){
+  //check each asteroid position for collision
+  var currentDistance;
+  var nodes = d3.selectAll("image");
+  hero.attr("fill", "orange");
+  d3.selectAll("image")
+    .each(function(d, i) {
+      var x = d3.select(this).attr("x");
+      var y = d3.select(this).attr("y");
+      currentDistance = distance(x, y);
+      if (currentDistance < enemyRadius + heroRadius - 10) {
+        hero.attr("fill", "green");
+        if (!collisionFlag) {
+          collisionCount++;
+          d3.select(".collisionCount").text(collisionCount);
+        }
+        collisionFlag = true;
+
+      }
+    });
+}, 10);
+
+var distance = function(enemyX, enemyY){
+  var heroX = hero.attr("cx");
+  var heroY = hero.attr("cy");
+  var a = Math.pow((heroX - enemyX), 2);
+  var b = Math.pow((heroY - enemyY), 2);
+  return Math.sqrt(a + b);
+}
+
+
 
 
